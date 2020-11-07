d97d15914b9f1803a8070ff52592a3f873b642a7
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-07 13:04:36 -0700

    Add discount column to merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_161751) do
+ActiveRecord::Schema.define(version: 2020_11_07_200300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_161751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: true
+    t.integer "discount"
   end
 
   create_table "order_items", force: :cascade do |t|
