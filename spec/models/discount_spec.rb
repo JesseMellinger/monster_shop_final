6e01af91bdfcf0c373219ed839dee982074560a3
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 18:56:14 -0700

    Add test for item threshold uniqueness

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Discount do
 
   describe 'Validations' do
     it {should validate_numericality_of(:item_threshold).only_integer}
+    it {should validate_uniqueness_of(:item_threshold)}
     it {should validate_numericality_of(:value)}
     it {should validate_presence_of :item_threshold}
     it {should validate_presence_of :value}
