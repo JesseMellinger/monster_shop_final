44f8ba7bb8ed3dcce5da46695e8f5a54d9e3928c
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 19:31:36 -0700

    Remove unnecessary discount column from merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_234147) do
+ActiveRecord::Schema.define(version: 2020_11_10_022740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_11_07_234147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: true
-    t.integer "discount"
   end
 
   create_table "order_items", force: :cascade do |t|
